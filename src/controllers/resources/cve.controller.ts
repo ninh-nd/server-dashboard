dec99321c60d7e8b8df95a885c21675666ed1d7e
ninh-nd <nguyendangninh0@gmail.com>
2023-01-10 19:52:55 +0700

    Remove leftover console.log

--- src/controllers/resources/cve.controller.ts
+++ src/controllers/resources/cve.controller.ts
@@ -13,7 +13,6 @@ async function get(req: Request, res: Response) {
     const cwes = data.cwes // Array of CWEs
     const vendor = Object.keys(data.vendors)[0]
     const product = data.vendors[`${vendor}`][0]
-    console.log(data.raw_nvd_data.configurations.nodes[0].cpe_match)
     const version = data.raw_nvd_data.configurations.nodes[0].cpe_match.map(({ cpe23Uri }: { cpe23Uri: string, vulnerable: boolean }) => cpe23Uri.split(':')[5])
     const returnObject = { cveId, desc, score, cwes, vendor, product, version }
     return res.status(200).json(successResponse(returnObject, "CVE fetched successfully"))
