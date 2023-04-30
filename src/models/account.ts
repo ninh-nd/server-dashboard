b5aa0d482f06c61337db38c41fca210139aaca52
Dang Ninh <ninh.nd194815@sis.hust.edu.vn>
2023-04-30 20:24:42 +0700

    Automatically assign permissions on account creation

--- src/models/account.ts
+++ src/models/account.ts
@@ -1,6 +1,7 @@
 import { model, Schema, Model } from "mongoose";
 import { IAccount } from "./interfaces";
 import { thirdPartySchema } from "./thirdParty";
+import permissions from "utils/permission";
 
 const accountSchema = new Schema<IAccount>({
   username: {
@@ -29,9 +30,31 @@ const accountSchema = new Schema<IAccount>({
   permission: [
     {
       type: String,
-      default: [],
+      required: true,
     },
   ],
+});
+accountSchema.pre("save", function (next) {
+  if (this.role === "admin") {
+    this.permission = permissions;
+  } else if (this.role === "manager") {
+    const perm = permissions.filter((p) => {
+      if (p.includes("user") || p.includes("account")) return false;
+    });
+    this.permission = perm;
+  } else {
+    const perm = permissions.filter((p) => {
+      if (
+        p.includes("user") ||
+        p.includes("account") ||
+        p.includes("phase") ||
+        p.includes("project")
+      )
+        return false;
+    });
+    this.permission = perm;
+  }
+  next();
 });
 const Account: Model<IAccount> = model("Account", accountSchema);
 
