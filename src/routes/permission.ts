74fb80292d8e584ddb03a4853430657746ce390e
Dang Ninh <ninh.nd194815@sis.hust.edu.vn>
2023-04-30 20:18:55 +0700

    Store permission list separately

--- src/routes/permission.ts
+++ src/routes/permission.ts
@@ -1,35 +1,7 @@
 import express from "express";
+import permissions from "utils/permission";
 import { successResponse } from "utils/responseFormat";
 const router = express.Router();
-const permissions = [
-  "ticket:create",
-  "ticket:read",
-  "ticket:update",
-  "ticket:delete",
-  "task:create",
-  "task:read",
-  "task:update",
-  "task:delete",
-  "artifact:create",
-  "artifact:read",
-  "artifact:update",
-  "artifact:delete",
-  "phase:create",
-  "phase:read",
-  "phase:update",
-  "phase:delete",
-  "project:create",
-  "project:read",
-  "project:update",
-  "project:delete",
-  "user:create",
-  "user:read",
-  "user:update",
-  "user:delete",
-  "account:read",
-  "account:update",
-  "account:delete",
-];
 router.get("/", (req, res) => {
   return res.json(
     successResponse(permissions, "Permissions fetched successfully")
