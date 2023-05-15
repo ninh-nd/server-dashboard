30f656a4e745de2c5b57bc54e8191fec16568129
Dang Ninh <ninh.nd194815@sis.hust.edu.vn>
2023-05-15 20:17:31 +0700

    Update on Threat model

--- src/models/threat.ts
+++ src/models/threat.ts
@@ -1,6 +1,39 @@
-import { prop } from "@typegoose/typegoose";
+import { pre, prop } from "@typegoose/typegoose";
 import { Base } from "@typegoose/typegoose/lib/defaultClasses";
 export interface Threat extends Base {}
+class DetailScore {
+  @prop({ type: Number, required: true })
+  public damage!: number;
+
+  @prop({ type: Number, required: true })
+  public reproducibility!: number;
+
+  @prop({ type: Number, required: true })
+  public exploitability!: number;
+
+  @prop({ type: Number, required: true })
+  public affectedUsers!: number;
+
+  @prop({ type: Number, required: true })
+  public discoverability!: number;
+}
+class Score {
+  @prop({ type: Number, required: true })
+  public total!: number;
+
+  @prop({ type: Number, required: true })
+  public details!: DetailScore;
+}
+@pre<Threat>("save", function (next) {
+  this.score.total =
+    (this.score.details.damage +
+      this.score.details.reproducibility +
+      this.score.details.exploitability +
+      this.score.details.affectedUsers +
+      this.score.details.discoverability) /
+    5;
+  next();
+})
 export class Threat {
   @prop({ required: true, type: String })
   public name!: string;
@@ -20,8 +53,19 @@ export class Threat {
       "Elevation of Privilege",
     ],
   })
-  public category!: string;
+  public type!: string;
 
   @prop({ type: String })
   public mitigation?: string;
+
+  @prop({ type: Score, required: true })
+  public score!: Score;
+
+  @prop({
+    type: String,
+    required: true,
+    enum: ["Non mitigated", "Partially mitigated", "Fully mitigated"],
+    default: "Non mitigated",
+  })
+  public status!: string;
 }
