b05b2545832b724940f654603cf2ca3162ee0e77
Dang Ninh <ninh.nd194815@sis.hust.edu.vn>
2023-05-29 22:14:23 +0700

    Add route to push workflow

--- src/routes/workflow.ts
+++ src/routes/workflow.ts
@@ -1,7 +1,12 @@
 import express from "express";
-import { getWorkflows } from "../controllers/workflow.controller";
+import {
+  getWorkflows,
+  pushNewWorkflow,
+} from "../controllers/workflow.controller";
 const workflowRoute = express.Router();
 
 workflowRoute.get("/", getWorkflows);
+
+workflowRoute.post("/", pushNewWorkflow);
 
 export default workflowRoute;
